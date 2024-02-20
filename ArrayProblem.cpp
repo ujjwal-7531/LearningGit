e4c32ab9d8e3c454174f017dd2630eefe7ec0076
shub-66 <152512666+shub-66@users.noreply.github.com>
2024-02-20 22:51:24 +0530

    Done with O(N)

--- ArrayProblem.cpp
+++ ArrayProblem.cpp
@@ -6,16 +6,28 @@
 using namespace std;
 int main(){
     int n;
-    cout<<"Enter size of the array";
+    cout<<"Enter size of the array: ";
     cin>>n;
-    cout<<"\nEnter elements(saperated with spaces):";
+    cout<<"\nEnter elements(saperated with spaces): ";
     vector <int> v(n);
     for(int i{0};i<n;++i){
         cin>>v[i];
     }
-
     //WRITE CODE BELOW FROM HERE.
-
+    //sample vector: {1,6,-3,6,-4,18}
+    int max_diff{0},ans=INT_MIN,sum{0},presum{0};
+    for(int i{0};i<v.size();++i){
+        sum+=v[i];
+    }
+    for(int i{0};i<v.size()-1;++i){
+        max_diff=0;
+        presum+=v[i];
+        max_diff=presum-(sum-presum);
+        if(max_diff<0)
+        max_diff*=(-1);
+        ans=max(ans,max_diff);
+    }
+    cout<<"Max difference is: "<<ans;
     //WRITE YOUR CODE ABOVE FROM HERE.
     return 0;
 }
