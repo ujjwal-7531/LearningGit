cc283ba1d601da693cf4fa4401a02de048e1d7ab
Ujjwal Narayan Thakur <narayanujjwal3107@gmai.com>
2024-02-20 21:14:30 +0530

    Forgot to add some part.

---
+++ ArrayProblem.cpp
@@ -0,0 +1,21 @@
+//divide a given array into two sub array with max difference
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<climits>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter size of the array";
+    cin>>n;
+    cout<<"\nEnter elements(saperated with spaces):";
+    vector <int> v(n);
+    for(int i{0};i<n;++i){
+        cin>>v[i];
+    }
+
+    //WRITE CODE BELOW FROM HERE.
+
+    //WRITE YOUR CODE ABOVE FROM HERE.
+    return 0;
+}
